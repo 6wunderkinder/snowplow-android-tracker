88bc09321d38cc358bbef7a72f119e5476f14deb
Johannes Orgis <hannes@6wunderkinder.com>
2015-02-18 16:46:13 +0100

    pulled wrapper method from android code (apache v2 license)

--- snowplow-android-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/utils/Util.java
+++ snowplow-android-tracker/src/main/java/com/snowplowanalytics/snowplow/tracker/utils/Util.java
@@ -13,6 +13,10 @@
 
 package com.snowplowanalytics.snowplow.tracker.utils;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.Collection;
 import java.util.Map;
 import java.util.UUID;
 
@@ -56,7 +60,82 @@ public class Util {
     }
 
     public static String serializeMap(Map map) {
-        return "";
+        return wrap(map).toString();
+    }
+
+
+    //methods below extracted from android implementation of  org.Json.JSONObject
+ /*
+ * Copyright (C) 2010 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+    /**
+     * A sentinel value used to explicitly define a name with no value. Unlike
+     * {@code null}, names with this value:
+     * <p>This value violates the general contract of {@link Object#equals} by
+     * returning true when compared to {@code null}. Its {@link #toString}
+     * method returns "null".
+     */
+    public static final Object NULL = new Object() {
+        @Override
+        public boolean equals(Object o) {
+            return o == this || o == null; // API specifies this broken equals implementation
+        }
+
+        @Override
+        public String toString() {
+            return "null";
+        }
+    };
+
+    public static Object wrap(Object o) {
+        if (o == null) {
+            return NULL;
+        }
+        if (o instanceof JSONArray || o instanceof JSONObject) {
+            return o;
+        }
+        if (o.equals(NULL)) {
+            return o;
+        }
+        try {
+            if (o instanceof Collection) {
+                return new JSONArray((Collection) o);
+            } else if (o.getClass().isArray()) {
+                return new JSONArray(o);
+            }
+            if (o instanceof Map) {
+                return new JSONObject((Map) o);
+            }
+            if (o instanceof Boolean ||
+                    o instanceof Byte ||
+                    o instanceof Character ||
+                    o instanceof Double ||
+                    o instanceof Float ||
+                    o instanceof Integer ||
+                    o instanceof Long ||
+                    o instanceof Short ||
+                    o instanceof String) {
+                return o;
+            }
+            if (o.getClass().getPackage().getName().startsWith("java.")) {
+                return o.toString();
+            }
+        } catch (Exception ignored) {
+        }
+        return null;
     }
 
 
