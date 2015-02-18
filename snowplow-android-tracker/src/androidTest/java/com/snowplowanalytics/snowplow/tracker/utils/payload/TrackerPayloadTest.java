ac3fa2dda0902f75233ee1d05b852f6091dc95db
Johannes Orgis <hannes@6wunderkinder.com>
2015-02-18 16:46:46 +0100

    AndroidUnitTest for TrackerPayload - compares to reference implementation

--- snowplow-android-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/utils/payload/TrackerPayloadTest.java
+++ snowplow-android-tracker/src/androidTest/java/com/snowplowanalytics/snowplow/tracker/utils/payload/TrackerPayloadTest.java
@@ -1,83 +1,182 @@
 package com.snowplowanalytics.snowplow.tracker.utils.payload;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.test.AndroidTestCase;
 
-import com.snowplowanalytics.snowplow.tracker.DevicePlatforms;
-import com.snowplowanalytics.snowplow.tracker.Emitter;
-import com.snowplowanalytics.snowplow.tracker.Subject;
-import com.snowplowanalytics.snowplow.tracker.Tracker;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.snowplowanalytics.snowplow.tracker.utils.payload.jackson.JacksonTrackerPayload;
+import junit.framework.ComparisonFailure;
 
 public class TrackerPayloadTest extends AndroidTestCase {
 
+
+    private static final HashMap<String, Object> parameterMap = new HashMap<>();
+
+    static {
+        parameterMap.put("param1key", "param1value");
+        parameterMap.put("param2key", true);
+        parameterMap.put("param3key", 42);
+    }
     // Helper Methods
 
-    private Tracker getTracker() {
-        // Make an emitter
-        Emitter emitter = new Emitter
-                .EmitterBuilder("testUrl", getContext())
-                .build();
+    private JacksonTrackerPayload getJacksonPayload() {
+        JacksonTrackerPayload payload = new JacksonTrackerPayload();
+        payload.add("dummyKey", "dummydata");
+        payload.add("parameterkey", parameterMap);
+        return payload;
+    }
 
-        Subject subject = new Subject(getContext());
-
-        // Make and return the Tracker object
-        return new Tracker
-                .TrackerBuilder(emitter, "myNamespace", "myAppId")
-                .subject(subject)
-                .platform(DevicePlatforms.InternetOfThings)
-                .base64(false)
-                .build();
+    private TrackerPayload getTrackerPayload() {
+        TrackerPayload payload = new TrackerPayload();
+        payload.add("dummyKey", "dummydata");
+        payload.add("parameterkey", parameterMap);
+        return payload;
     }
 
     // Tests
 
-    public void testNamespaceSet() {
-        Tracker tracker = getTracker();
-        assertEquals("myNamespace", tracker.getNamespace());
+    public void testToString() {
+        JacksonTrackerPayload payload = getJacksonPayload();
+        TrackerPayload trackerPayload = getTrackerPayload();
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
     }
 
-    public void testAppIdSet() {
-        Tracker tracker = getTracker();
-        assertEquals("myAppId", tracker.getAppId());
+
+    public void testAddString() {
+        JacksonTrackerPayload payload = getJacksonPayload();
+        TrackerPayload trackerPayload = getTrackerPayload();
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        String value = "testAddedString";
+        String key = "testAddedKey";
+        payload.add(key, value);
+        assertFalse(payload.toString().equals(trackerPayload.toString()));
+        trackerPayload.add(key, value);
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        assertTrue(payload.toString().contains(key));
+        assertTrue(payload.toString().contains(value));
     }
 
-    public void testDevicePlatformSet() {
-        Tracker tracker = getTracker();
-        assertEquals(DevicePlatforms.InternetOfThings, tracker.getPlatform());
+
+    public void testAddObjectBooelan() {
+        JacksonTrackerPayload payload = getJacksonPayload();
+        TrackerPayload trackerPayload = getTrackerPayload();
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        Object value = false;
+        String key = "testAddedKeyBoolean";
+        payload.add(key, value);
+        assertFalse(payload.toString().equals(trackerPayload.toString()));
+        trackerPayload.add(key, value);
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        assertTrue(payload.toString().contains(key));
+        assertTrue(payload.toString().contains(String.valueOf(value)));
     }
 
-    public void testBase64Set() {
-        Tracker tracker = getTracker();
-        assertEquals(false, tracker.getBase64Encoded());
+    public void testAddObjectInt() {
+        JacksonTrackerPayload payload = getJacksonPayload();
+        TrackerPayload trackerPayload = getTrackerPayload();
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        Object value = 2277812;
+        String key = "testAddedKeyInt";
+        payload.add(key, value);
+        assertFalse(payload.toString().equals(trackerPayload.toString()));
+        trackerPayload.add(key, value);
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        assertTrue(payload.toString().contains(key));
+        assertTrue(payload.toString().contains(String.valueOf(value)));
     }
 
-    public void testEmitterSet() {
-        Tracker tracker = getTracker();
-        assertNotNull(tracker.getEmitter());
+
+    public void testAddMapSimple() {
+        JacksonTrackerPayload payload = getJacksonPayload();
+        TrackerPayload trackerPayload = getTrackerPayload();
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        Map<String, Object> value = new HashMap<>();
+        value.put("k1string", "hello world");
+        value.put("k2boolean", true);
+        value.put("k3Int", 9283764);
+        payload.addMap(value);
+        assertFalse(payload.toString().equals(trackerPayload.toString()));
+        trackerPayload.addMap(value);
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        assertTrue(payload.toString().contains("k1string"));
+        assertTrue(payload.toString().contains("k2boolean"));
+        assertTrue(payload.toString().contains("k3Int"));
+        assertTrue(payload.toString().contains("hello world"));
+        assertTrue(payload.toString().contains(String.valueOf(true)));
+        assertTrue(payload.toString().contains(String.valueOf(9283764)));
     }
 
-    public void testSubjectSet() {
-        Tracker tracker = getTracker();
-        assertNotNull(tracker.getSubject());
+
+    public void testAddMapNoBase64() {
+        JacksonTrackerPayload payload = getJacksonPayload();
+        TrackerPayload trackerPayload = getTrackerPayload();
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        Map<String, Object> value = new HashMap<>();
+        value.put("k1string", "hello world");
+        value.put("k2boolean", true);
+        value.put("k3Int", 9283764);
+        payload.addMap(value, false, "keyb64", "keyNob64");
+        assertFalse(payload.toString().equals(trackerPayload.toString()));
+        trackerPayload.addMap(value, false, "keyb64", "keyNob64");
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        assertTrue(payload.toString().contains("k1string"));
+        assertTrue(payload.toString().contains("k2boolean"));
+        assertTrue(payload.toString().contains("k3Int"));
+        assertTrue(payload.toString().contains("hello world"));
+        assertFalse(payload.toString().contains("keyb64"));
+        assertTrue(payload.toString().contains("keyNob64"));
+        assertTrue(payload.toString().contains(String.valueOf(true)));
+        assertTrue(payload.toString().contains(String.valueOf(9283764)));
     }
 
-    public void testVersionSet() {
-        Tracker tracker = getTracker();
-        assertEquals("andr-0.3.0", tracker.getTrackerVersion());
+
+    public void testAddMapBase64() {
+        JacksonTrackerPayload payload = getJacksonPayload();
+        TrackerPayload trackerPayload = getTrackerPayload();
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        Map<String, Object> value = new HashMap<>();
+        value.put("k1string", "hello world");
+        value.put("k2boolean", true);
+        value.put("k3Int", 9283764);
+        payload.addMap(value, true, "keyb64", "keyNob64");
+        assertFalse(payload.toString().equals(trackerPayload.toString()));
+        trackerPayload.addMap(value, true, "keyb64", "keyNob64");
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
+        assertFalse(payload.toString().contains("k1string"));
+        assertFalse(payload.toString().contains("k2boolean"));
+        assertFalse(payload.toString().contains("k3Int"));
+        assertFalse(payload.toString().contains("hello world"));
+        assertTrue(payload.toString().contains("keyb64"));
+        assertFalse(payload.toString().contains("keyNob64"));
+        assertFalse(payload.toString().contains(String.valueOf(9283764)));
     }
 
-    public void testSubjectUpdate() {
-        Tracker tracker = getTracker();
-        assertNotNull(tracker.getSubject());
-
-        tracker.setSubject(null);
-        assertNull(tracker.getSubject());
+    public void testGetMap() {
+        JacksonTrackerPayload payload = getJacksonPayload();
+        TrackerPayload trackerPayload = getTrackerPayload();
+        assertJsonEquals(payload.toString(), trackerPayload.toString());
     }
 
-    public void testPlatformUpdate() {
-        Tracker tracker = getTracker();
-        assertEquals(DevicePlatforms.InternetOfThings, tracker.getPlatform());
-
-        tracker.setPlatform(DevicePlatforms.Mobile);
-        assertEquals(DevicePlatforms.Mobile, tracker.getPlatform());
+    private void assertJsonEquals(String json1, String json2) throws ComparisonFailure {
+        try {
+            JSONObject ob1 = new JSONObject(json1);
+            JSONObject ob2 = new JSONObject(json2);
+            assertEquals(ob1.toString(), ob2.toString());
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode node1 = mapper.readTree(json1);
+            JsonNode node2 = mapper.readTree(json2);
+            assertEquals(node1, node2);
+        } catch (JSONException | IOException e) {
+            e.printStackTrace();
+            throw new ComparisonFailure(e.getMessage(), "JsonObject", "could not create");
+        }
     }
+
 }
